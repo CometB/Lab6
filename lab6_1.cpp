271d2bbe49c79aa7d1143173920a769fc2749c68
CometB <rpila23461@gmail.com>
2024-12-02 21:42:02 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,30 @@
 #include<iostream>
+
 using namespace std;
 
-int main(){
+int n;
+
+void inputN(){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> n;
+}
+
+int main(){
+    
+    int even = 0;
+    int odd = 0;
+    
+    inputN();
+    while(n != 0){
+        if(n%2 == 0){
+            even++;
+        } else{
+            odd++;
+        }
+        inputN();
+    }
+    
+    cout << "#Even numbers = " << even << '\n';
+    cout << "#Odd numbers = " << odd << '\n';
     return 0;
 }
